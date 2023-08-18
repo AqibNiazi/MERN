ce6fb33d6993625bec1d4f441a302dd6248f844d
Muhammad Aqib Javed <91584862+AqibNiazi@users.noreply.github.com>
2023-08-18 22:53:30 +0500

    html templates updated

--- Node_Express_Mongo/starter/index.js
+++ Node_Express_Mongo/starter/index.js
@@ -1,41 +1,49 @@
 const fs = require("fs");
 const http = require("http");
-const { json } = require("node:stream/consumers");
 const url = require("url");
 // //Blocking,synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 // const textout = `This is what we know about Avacado : ${textIn}`;
 // fs.writeFileSync("./txt/output.txt", textout);
 // console.log("file written");
+
 //Non-Blocking,Asynchronous way
-// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-//   if (err) return console.log("ERROR!");
-//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     console.log(data2);
-//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-//       console.log(data3);
-//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-//         console.log("your files have been written");
-//       });
-//     });
-//   });
-// });
-// console.log("will read file!");
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR!");
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("your files have been written");
+      });
+    });
+  });
+});
+console.log("will read file!");
 
 // Setting up a server
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const dataObject = JSON.parse(data);
+const dataObject = JSON.parse(data); //convert json data into object
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
+
+// Overview page
   if (pathName === "/" || pathName === "/overview") {
     res.end("This is overview");
+
+// Product page
   } else if (pathName === "/product") {
     res.end("this is product");
+
+// Api
   } else if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
-
     res.end(data);
+
+// Page Not found
   } else {
     res.writeHead(404, {
       contentType: "text/html",
